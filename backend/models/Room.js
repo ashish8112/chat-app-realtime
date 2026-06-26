b7a7cf0685e5d07d91bc7dfe5a51c36fdf6db9d9
Ashish <ashish233227shukla@gmail.com>
2026-06-26 20:48:48 +0530

    model for Room coilection

---
+++ backend/models/Room.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const roomSchema = new mongoose.Schema({
+    name:{type:String,required:true,unique:true},
+    description:{type:String,default:""},
+    createdBy:{type:mongoose.Schema.Types.ObjectId,ref:"User"},
+    members:[{type:mongoose.Schema.Types.ObjectId,ref:"User"}],
+    isPrivate:{type:Boolean,default:false}
+},{timestamps:true})
+
+const Room = mongoose.model("Room",roomSchema);
+
+module.exports=Room;
