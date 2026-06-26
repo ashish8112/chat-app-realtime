ad24d535e0e8e67dc88eecce95f89b632503ac2f
Ashish <ashish233227shukla@gmail.com>
2026-06-26 10:03:11 +0530

    created server.js

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const {Server} = require("socket.io");
+const connectDB = require("./config/db");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.json());
+
+
+app.use((req,res)=>{
+    res.status(404).json({message:"Enter the correct url"})
+})
+
+app.use((err,req,res,next)=>{
+    console.error(err.message);
+    return res.status(500).json({message:err.message})
+})
+
+
+
+connectDB();
+
+server.listen(process.env.PORT,()=>{
+    console.log("Server Started at PORT= "+process.env.PORT);
+})
