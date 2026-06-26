93f862246776f07eb427d2b98f4679329431d2bc
Ashish <ashish233227shukla@gmail.com>
2026-06-26 22:52:30 +0530

    Updated User.js with middleware

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
     username:{type:String,required:true,unique:true},
@@ -9,6 +10,13 @@ const userSchema = new mongoose.Schema({
     isOnline:{type:Boolean,default:false},
     lastSeen:{type:Date,default:Date.now},
 },{timestamps:true})
+
+userSchema.pre("save",async function (next){ // don't use arrow function
+    if(!this.isModified("password")) // user.save() time password new hain phir bhi, will marked as modified
+        return next();
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+})
 
 const User = mongoose.model("User",userSchema);
 
