894dc7c391c5c9f0406ccc568e29f58b5c9324f5
Ashish <ashish233227shukla@gmail.com>
2026-06-27 21:36:34 +0530

    updated message routes

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -2,6 +2,7 @@ const Room = require("../models/Room");
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware")
+const Message = require("../models/Message")
 
 router.get("/",async(req,res)=>{
     try{
@@ -24,6 +25,25 @@ router.post("/",authMiddleware,async(req,res)=>{
         const room = new Room ({name,description,createdBy:req.user.id,members:[req.user.id],isPrivate})
         await room.save();
         return res.status(201).json(room);
+    }
+    catch(err){
+        return res.status(500).json({message:err.message})
+    }
+})
+
+router.get("/:roomId/messages",authMiddleware,async(req,res)=>{
+    try{
+        const {roomId} = req.params;
+        const roomCheck = await Room.findById(roomId);
+        if(!roomCheck)
+            return res.status(404).json({message:"Room doesn't exist"})
+        const messages = (await Message.find({room:roomId,type:"room"})
+        .populate("sender","username avatar")
+        .sort({createdAt:-1})//descending latest message to previous messsage  
+        .limit(50));
+        if(messages.length===0)
+            return res.status(404).json({message:"No message history"});
+        return res.status(200).json(messages.reverse()); // because message is stored new to old so new is last and old is lates in messages object so need to reverse
     }
     catch(err){
         return res.status(500).json({message:err.message})
