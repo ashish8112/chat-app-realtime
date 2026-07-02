8edb577c4fbc43b1c47cac09da71f895759686fb
Ashish <ashish233227shukla@gmail.com>
2026-07-02 21:34:18 +0530

    implementd socket.io-client

---
+++ frontend/src/socket.js
@@ -0,0 +1,13 @@
+import {io} from "socket.io-client"
+const url = import.meta.env.VITE_API_URL || "http://localhost:3000"
+let socket;
+export const connectSocket = (token) => {
+    socket = io(url,{auth:{token}})
+    return socket; // to provide socket object to calling method, if it needs.
+}
+export const getSocket = () => socket;
+
+export const disconnectSocket = () =>{
+    if(socket) socket.disconnect();
+    
+}
