14e38c88226daaa265d3b9aa03d0c9e77c1ae1f3
Ashish <ashish233227shukla@gmail.com>
2026-07-02 17:43:32 +0530

    fixed port typo from 300 to 3000 for backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API = axios.create({baseURL:import.meta.env.VITE_API_URL || "http://localhost:300/api"})
+const API = axios.create({baseURL:import.meta.env.VITE_API_URL || "http://localhost:3000/api"})
 API.interceptors.request.use((config)=>{
     const user = JSON.parse(localStorage.getItem("chatUser"));
     if(user?.token)config.headers.Authorization=`Bearer ${user.token}`
