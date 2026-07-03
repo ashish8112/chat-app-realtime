b94dd4bbe9fa9bf56128f5cc1a7856a17e67d889
Ashish <ashish233227shukla@gmail.com>
2026-07-03 22:04:56 +0530

    added some button for routing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,12 @@
+import { useNavigate } from "react-router-dom"
 export default function Home(){
+    const navigate = useNavigate();
     return (
+        <>
         <h1>Home</h1>
+        <button onClick={()=>navigate("/register")}>Register</button>
+        <br></br>
+        <button onClick={()=>navigate("/login")}>Login</button>
+        </>
     )
 }
