90766ecddabcf959d9d4aff057ee0e15b286ae59
Ashish <ashish233227shukla@gmail.com>
2026-07-03 22:04:38 +0530

    import app.css and register route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes,Route } from "react-router-dom"
 import Login from "./pages/Login"
 import Home from "./pages/Home"
+import './app.css'; 
+import Register from "./pages/Register";
 export default function App(){
   return(
     <>
       <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
       </Routes>
     </>
   )
