aeecbc3693ce28b79b63a061c8a7931a4d90ce47
Ashish <ashish233227shukla@gmail.com>
2026-07-03 10:03:38 +0530

    changed some typos

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { useState } from "react";
+import '../app.css'; 
+
 export default function Login(){
-    const [fromData,setFormData] = useState({
+    const [formData,setFormData] = useState({
         email:"",
         password:""
     })
@@ -15,15 +17,15 @@ export default function Login(){
     }
     return (
         <>
-        <div className="loginContainter">
+        <div className="loginContainer">
         <form onSubmit={handleSubmit} className="login-form">
         <h2>Welcome Back</h2>
         <div className="input-group">
-            <input type="text" name="email" value={FormData.email} onChange={handleChange}/>
+            <input type="text" name="email" value={formData.email} onChange={handleChange}/>
             <label>Username</label>
         </div>
         <div className="input-group">
-            <input type="password" name="password" value={FormData.password} onChange={handleChange} />
+            <input type="password" name="password" value={formData.password} onChange={handleChange} />
             <label>Password</label>
         </div>
         <button className="login-btn" type="submit">Log In</button>
