b2ec8c29c406005f031b5c70fade491bc9b48a7c
Ashish <ashish233227shukla@gmail.com>
2026-07-05 20:06:00 +0530

    added createRoom in sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,30 +1,33 @@
 import { useState,useEffect } from "react"
 import API from "../api/axios"
+import CreateRoom from "./CreateRoom";
+
 export default function Sidebar({onRoomSelect}){
     const [rooms,setRooms] = useState([]);
     const [loading,setLoading] = useState(true);
-    useEffect(()=>{
-        async function getRooms(){
+      async function getRooms(){
             try{
                 const {data} = await API.get("/rooms/");
                 setRooms(data);
                 setLoading(false);
             }
             catch(err){
-                alert(err.response?.data?.message||"Failed to Fetch Rooms");
                 setLoading(false);
+                alert(err.response?.data?.message||"Failed to Fetch Rooms");   
             }
         }
+    useEffect(()=>{
+      
         getRooms();
     },[])
     if(loading)
         return <p>Loading ....</p>
     return (
         <div className="sidebar-container">
-        <div id="sidebar-heading">
-        <h1 id="public-heading">Public Chat</h1>
-        <button id="public-btn">Create Room</button>
-        </div>
+            <div id="sidebar-heading">
+                <h1 id="public-heading">Public Chat</h1>
+                <CreateRoom onRoomCreated={getRooms}/>
+            </div>
         
         <ul className="publicRoom-container">
         {rooms.map((room)=>(
@@ -33,6 +36,13 @@ export default function Sidebar({onRoomSelect}){
             </li>
         ))}
         </ul>
+            <div className="sidebar-secondaryHeading">
+                <h1 className="private-heading" style={{textAlign:"center"}}>Private Chat</h1>
+                <ul className="privateRoom-container">
+
+                </ul>
+            </div>
+        
         </div>
     )
 }
