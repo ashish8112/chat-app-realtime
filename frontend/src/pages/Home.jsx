efeb043b5a757fff1824c755247e053ebdb40dd5
Ashish <ashish233227shukla@gmail.com>
2026-07-05 20:06:20 +0530

    Updated some coments

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import { useNavigate } from "react-router-dom"
+import Sidebar from "../components/Sidebar"
+import ChatWindow from "../components/ChatWindow";
+import { useState } from "react";
 export default function Home(){
     const navigate = useNavigate();
+    const [selectedRoom,setSelectedRoom] = useState(null);
     return (
         <>
-        <h1>Home</h1>
-        <button onClick={()=>navigate("/register")}>Register</button>
-        <br></br>
-        <button onClick={()=>navigate("/login")}>Login</button>
+        <div className="home-container">
+        <Sidebar onRoomSelect={setSelectedRoom}/>
+        <ChatWindow room={selectedRoom}/>
+        </div>
         </>
     )
 }
+
+// send setSelectedRoom method in sidebar as props so we can change selectedRoom from there
+//when selectedRoom changed because of setSelectedRoom method in sidebar this chatwindow 
+// will re render because of selectedRoom state in that.
