2a344a20fa622dc7c62e4a1465cc8c6f4e3d5530
Ashish <ashish233227shukla@gmail.com>
2026-07-05 20:05:03 +0530

    added route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,16 @@ import Login from "./pages/Login"
 import Home from "./pages/Home"
 import './app.css'; 
 import Register from "./pages/Register";
+import PortectedRoute from "./protectedRoute/ProtectedRoute";
 export default function App(){
   return(
     <>
       <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={
+            <PortectedRoute>
+              <Home/>
+            </PortectedRoute>
+          }/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/register" element={<Register/>}/>
       </Routes>
