c95925c734958d2c8ecb4bdf2c5c37508c599511
Ashish <ashish233227shukla@gmail.com>
2026-07-11 10:30:08 +0530

    updated comment

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import Sidebar from "../components/Sidebar"
 import ChatWindow from "../components/ChatWindow";
 import { useState } from "react";
-import {ErrorBoundary,getErrorMessage} from "react-error-boundary"
+// import {ErrorBoundary,getErrorMessage} from "react-error-boundary"
+
 export default function Home(){
     const [selectedRoom,setSelectedRoom] = useState(null);
     const [selectedUser,setSelectedUser] = useState(null);
@@ -10,13 +11,6 @@ export default function Home(){
         setSelectedUser(null);
     }
     return (
-         <ErrorBoundary fallbackRender={({ error, resetErrorBoundary }) => (
-            <div role="alert">
-            <p>Something went wrong:</p>
-            <pre>{getErrorMessage(error)}</pre>
-            <button onClick={resetErrorBoundary}>Try again</button>
-            </div>
-            )}>
             <div className="home-container">
                 <div className={selectedRoom||selectedUser ? "hide-mobile":""}>
                     <Sidebar onRoomSelect={setSelectedRoom} onUserSelect={setSelectedUser}/>
@@ -25,7 +19,6 @@ export default function Home(){
                     <ChatWindow room={selectedRoom} user={selectedUser} onClose={resetChat}/>
                 </div>
             </div>
-         </ErrorBoundary>
     )
 }
  //if group chat or direct message is selected display none of sidebar .and if both are not selected display none chatwindow
@@ -33,3 +26,20 @@ export default function Home(){
 // send setSelectedRoom method in sidebar as props so we can change selectedRoom from there
 //when selectedRoom changed because of setSelectedRoom method in sidebar this chatwindow 
 // will re render because of selectedRoom state in that.
+
+// <ErrorBoundary fallbackRender={({ error, resetErrorBoundary }) => (   //I used this previously but not now created another component to make it more reusable check app.jsx
+//             <div role="alert">
+//             <p>Something went wrong:</p>
+//             <pre>{getErrorMessage(error)}</pre>
+//             <button onClick={resetErrorBoundary}>Try again</button>
+//             </div>
+//             )}>
+// <div className="home-container">
+//                <div className={selectedRoom||selectedUser ? "hide-mobile":""}>
+//                    <Sidebar onRoomSelect={setSelectedRoom} onUserSelect={setSelectedUser}/>
+//                </div>
+//                <div className={!selectedRoom&&!selectedUser?"hide-mobile":""}>
+//                    <ChatWindow room={selectedRoom} user={selectedUser} onClose={resetChat}/>
+//                </div>
+//            </div> 
+// </ErrorBoundary>
